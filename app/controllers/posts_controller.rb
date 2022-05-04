fdf516e4ccbacb8a92802eac795124d31fa2461c
ginohmk <michotall95@gmail.com>
2022-05-04 22:36:37 +0100

    Add functionalities New post New comment New likes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,25 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @user = User.find(params[:user_id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.new(allowed_params)
+    if @post.save
+      @post.update_counter(params[:user_id])
+      redirect_to user_posts_path(@user.id)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def allowed_params
+    params.require(:post).permit(:title, :text, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show new create] do
+      resources :likes, only: %i[new create]
+      resources :comments, only: %i[new create]
+    end
   end
+  # get 'users/:user_id/posts/:id', to: 'comments#new', as: 'new_user_comment'
+  # post 'users/:user_id/posts/:id', to: 'comments#create'
+
+  # get 'users/:user_id/posts/:id', to: 'likes#new', as: 'new_user_like'
+  # post 'users/:user_id/posts/:id', to: 'likes#create'
 end
