bef62a3f8e927f6131003326ccff4b1f8de0920e
ginohmk <michotall95@gmail.com>
2022-05-04 23:01:54 +0100

    Remove comment

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,4 @@ Rails.application.routes.draw do
       resources :comments, only: %i[new create]
     end
   end
-  # get 'users/:user_id/posts/:id', to: 'comments#new', as: 'new_user_comment'
-  # post 'users/:user_id/posts/:id', to: 'comments#create'
-
-  # get 'users/:user_id/posts/:id', to: 'likes#new', as: 'new_user_like'
-  # post 'users/:user_id/posts/:id', to: 'likes#create'
 end
