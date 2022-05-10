7e95b7e02444bc2de7e4e2db4477f0a5e306634a
ginohmk <michotall95@gmail.com>
2022-05-10 23:43:51 +0100

    Add authorization

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', sign_up: 'register' }
 
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create] do
+    resources :posts, only: %i[index show new create destroy edit] do
       resources :likes, only: %i[create]
-      resources :comments, only: %i[create]
+      resources :comments, only: %i[create destroy]
     end
   end
 
