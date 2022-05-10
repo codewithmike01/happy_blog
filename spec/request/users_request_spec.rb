32e0972327fd62d7da7e349f8e361ca98a588474
ginohmk <michotall95@gmail.com>
2022-05-10 19:30:07 +0100

    Fix linters

--- spec/request/users_request_spec.rb
+++ spec/request/users_request_spec.rb
@@ -17,8 +17,11 @@ RSpec.describe 'Users', type: :request do
   end
 
   context 'User/show' do
-    current_user = User.new(name: 'Franklyn',
-                            photo: 'https://images.unsplash.com/photo-1651517824434-6765ef914960?ixlib', bio: 'He schooled in campain high school', posts_counter: 4, email: 'hello@gmail.com', email_confirmed: true, confirm_token: 'jdhdjdhz7zh')
+    current_user = User.new(
+      name: 'Franklyn', photo: 'https://images.unsplash.com/photo-1651517824434-6765ef914960?ixlib',
+      bio: 'He schooled in campain high school', posts_counter: 4, email: 'hello@gmail.com',
+      email_confirmed: true, confirm_token: 'jdhdjdhz7zh'
+    )
     current_user.save
     before(:example) { get "/users/#{current_user.id}" } # get(:user#show)
 
